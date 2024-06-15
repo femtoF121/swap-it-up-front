371baa2ec058eba1f6cc10a407883dad84960df8
femtoF121 <femtoF121@users.noreply.github.com>
2024-06-15 20:43:59 +0000

    deploy: 0889ed21bc2d8a58e787cf27d50b320136fe3900

--- assets/index-BJSSQJRQ.js
+++
@@ -1,1 +0,0 @@
-import{j as s,i as n,c as o,ab as c}from"./index-BVKVkcm9.js";const x=({to:e,className:r,children:a,...t})=>s.jsxs(n,{...t,className:o("flex gap-2 items-center",r),to:e,children:[s.jsx(c,{}),a]});export{x as R};
