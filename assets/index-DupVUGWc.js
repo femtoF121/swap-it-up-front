c63dab9de3c5abf21521f1ae17c1afd2a6a5006f
femtoF121 <femtoF121@users.noreply.github.com>
2024-06-15 21:31:17 +0000

    deploy: 4cb336dbd4c46853f0a2a7ce20d5ffbe651a4a31

---
+++ assets/index-DupVUGWc.js
@@ -0,0 +1,1 @@
+import{j as s,i as n,c as o,ab as c}from"./index-D5iiHLqA.js";const x=({to:e,className:r,children:a,...t})=>s.jsxs(n,{...t,className:o("flex gap-2 items-center",r),to:e,children:[s.jsx(c,{}),a]});export{x as R};
--- assets/with-auth-Bk8tUVSr.js
+++
@@ -1,1 +0,0 @@
-import{r as a,K as i,j as t,L as u,a5 as c,R as x}from"./index-C64Kg7V9.js";const h=a.createContext({}),m=s=>r=>{const{data:e,isLoading:n,error:o}=i();return n?t.jsx("div",{className:"w-full min-h-screen flex justify-center items-center",children:t.jsx(u,{})}):o||!e?t.jsx(c,{to:x.SIGN_IN}):t.jsx(h.Provider,{value:e,children:t.jsx(s,{...r})})},d=m;export{h as U,d as w};
